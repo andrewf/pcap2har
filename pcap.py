0c37e1e6f4c9746ff062609185f0a87802193042
Andrew Fleenor <andrewflnr@gmail.com>
2010-07-19 18:09:38 -0700

    comments in pcap.friendly_tcp_flags

--- pcap.py
+++ pcap.py
@@ -85,6 +85,7 @@ def friendly_tcp_flags(flags):
     d = {dpkt.tcp.TH_FIN:'FIN', dpkt.tcp.TH_SYN:'SYN', dpkt.tcp.TH_RST:'RST', dpkt.tcp.TH_PUSH:'PUSH', dpkt.tcp.TH_ACK:'ACK', dpkt.tcp.TH_URG:'URG', dpkt.tcp.TH_ECE:'ECE', dpkt.tcp.TH_CWR:'CWR'}
     #make a list of the flags that are activated
     active_flags = filter(lambda t: t[0] & flags, d.iteritems()) #iteritems (sortof) returns a list of tuples
+    #join all their string representations with '|'
     return '|'.join(t[1] for t in active_flags)
 
 def viewtcp(pkts):
