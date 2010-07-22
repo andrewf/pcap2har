8f04164d49d1ae8cea522cc948d78329af6ba35a
Andrew Fleenor <andrewflnr@gmail.com>
2010-07-22 15:27:10 -0700

    possibly faulty tcp seq # comp algo

--- tcpseq.py
+++ tcpseq.py
@@ -1,13 +1,29 @@
 
 
 def twos_comp(x):
-    return ~(x-1)
+    return (~x)+1
+
+numberspace = 2**32 # seq numbers are up to but not including this
+halfspace = numberspace / 2
+
+def wrap(x):
+    '''emulates the cast to int used in the C tcp seq # subtraction algo:
+    (int)( (a) - (b) ). Basically, if a number's absolute value is greater
+    than half the (unsigned) number space, it needs to be wrapped'''
+    # if abs(x) > numberspace / 2, its value must be reduced by numberspace/2,
+    # and its sign must be flipped
+    if x > halfspace:
+        x = 0 - (x - halfspace)
+    elif x < -halfspace:
+        x = 0 - (x + halfspace)
+    # x is now normalized
+    return x
 
 def subtract(a, b):
-    '''Calculate the difference between a and b, two python integers,
+    '''Calculate the difference between a and b, two python longs,
     in a manner suitable for comparing two TCP sequence numbers in a
     wrap-around-sensitive way.'''
-    return (a + twos_comp(b)) & 0xffffffff
+    return wrap(a - b)
 
 def lt(a, b):
     return subtract(a, b) < 0
@@ -18,14 +34,31 @@ class TestTcpSeqSubtraction(unittest.TestCase):
     def testNormalSubtraction(self):
         self.assertEqual(subtract(500L, 1L), 499L)
         self.assertEqual(subtract(1L, 1L), 0L)
-        self.assertEqual(subtract(0xffffffffL, 1L), 0xfffffffeL)
+        self.assertEqual(subtract(0x10000000, 0x20000000), -0x10000000)
         #self.assertEqual(subtract(20L, 0x
     def testWrappedSubtraction(self):
-        self.assertEqual(subtract(0, 0xffffffff), 1)
-        self.assertEqual(subtract(0xffffffff, 0xfffffffe), 1)
-        self.assertEqual(subtract(0, 1), -1)
-        
+        #self.assertEqual(subtract(0, 0xffffffff), 1)
+        # actual: a < b. want: a > b
+        self.assertEqual(subtract(0x10000000, 0xd0000000), 0x40000000)
+        # actual: a > b. want: a < b
+        self.assertEqual(subtract(0xd0000000, 0x10000000), -0x40000000)
+        print 'testing ryans case, subtraction'
+        self.assertEqual(subtract(0x100, 0xffffffff), -0x100)
+        #self.assertEqual(subtract(
+        #self.assertEqual(subtract(
+        #self.assertEqual(subtract(
+
+class TestLessThan(unittest.TestCase):
+    def testLessThan(self):
+        self.assertTrue( not lt(100, 10))
+        self.assertTrue( lt(0x100, 0xffffffff))
+        self.assertTrue( lt(0xf0000000, 0x7fffffff))
+    
 def runtests():
-    TcpSeqTestSuite = unittest.defaultTestLoader.loadTestsFromTestCase(TestTcpSeqSubtraction)
+    suite = unittest.TestSuite()
+    suite.addTest(TestTcpSeqSubtraction("testNormalSubtraction"))
+    suite.addTest(TestTcpSeqSubtraction("testWrappedSubtraction"))
+    suite.addTest(TestLessThan("testLessThan"))
+    #suite.addTest(TestLessThan(""))
     runner = unittest.TextTestRunner()
-    runner.run(TcpSeqTestSuite)
+    runner.run(suite)
